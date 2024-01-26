74cc16b3e874cd0b7b8055539345b507b38a27dc
Munkiestar <munkieryder@gmail.com>
2024-01-26 09:57:27 +0100

    add/components structure folder, assest images

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,21 +2,21 @@ import {ClerkProvider} from '@clerk/nextjs';
 import React from 'react';
 import {Inter} from 'next/font/google';
 
-export const metadata ={
+export const metadata = {
   title: 'Threads',
-  description:'A Next.js 14 Meta Threads App'
-}
+  description: 'A Next.js 14 Meta Threads App'
+};
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({subsets: ['latin']});
 
-export default function RootLayout({children}:{children:React.ReactNode}){
-   return (
-     <ClerkProvider>
-     <html lang="en">
-     <body className={`${inter.className} bg-dark-1`}>
-     {children}
-     </body>
-     </html>
-   </ClerkProvider>
-   )
+export default function RootLayout({children}: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+      <body className={`${inter.className} bg-dark-1`}>
+      {children}
+      </body>
+      </html>
+    </ClerkProvider>
+  );
 }
